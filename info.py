dd926fdd2fe78f512711c21ce13000f79a445e46
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-02-01 20:11:53 +0530

    input output and Logical Operator

---
+++ info.py
@@ -0,0 +1,9 @@
+#input / output
+name=input("Enter Your Name:")
+g=int(input("Enter DOB :"))
+city=input("Enter City:")
+college=input("Enter Your College:")
+fav_color=input("Fav Color:")
+fav_animal=input("Fav Animal:")
+
+print(name,g,city,college,fav_color,fav_animal)
---
+++ si.py
@@ -0,0 +1,6 @@
+p=int(input("Principle="))
+r=int(input("Rate="))
+t=int(input("Time="))
+si=(p*r*t)
+
+print(si)
