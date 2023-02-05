c12783aaf4446225f23e3a01d1ce0d8cb2afbb80
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-02-05 22:04:51 +0530

    program to calculate area & perimeter of a circle

---
+++ AreaOfaCircle.py
@@ -0,0 +1,7 @@
+#program to find the area and perimeter of a circle
+
+r=int(input("Enter the radius of the circle :"))
+area=3.14*r*r
+perimeter=2*r*3.14
+print("Area of the circle is : ",area)
+print("Perimeter of the circle is : ",perimeter)
