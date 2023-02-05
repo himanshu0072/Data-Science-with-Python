48de1adb04e5dd078e7eb4f7a97209639e879ce2
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-02-05 22:18:52 +0530

    Applying some DSA algo in a list

---
+++ List.py
@@ -0,0 +1,19 @@
+# Creating a FruitsList with
+# the use of multiple values
+FruitsList = ["Mango", "Banana", "cherry", "Papaya", "Grapes",
+              "Pomegranate", "Lychee", "Watermelon", "Strawberry", "Plum","Kiwi","Lime","Lemon"]
+print("\nList containing Fruits name: ")
+print(FruitsList)
+
+# accessing a element from the FruitsList using index number
+print("Accessing element from the FruitsList")
+print("The First Fruit of the List is:",FruitsList[0])
+print("The 2nd Fruit of the List is:",FruitsList[2])
+
+
+print("Accessing element using negative indexing")
+# print the last element of FruitsList
+print("The Last Fruit of the List is:",FruitsList[-1])
+
+# print the third last element of FruitsList
+print("The 3rd Last Fruit of the List is:",FruitsList[-3])
