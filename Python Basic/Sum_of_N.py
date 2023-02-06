b89349d74b2f397894a8ab8ae35ae3c84072373c
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-02-06 14:04:33 +0530

    Pythn Basic folder added with sum and table file

---
+++ Python Basic/Sum_of_N.py
@@ -0,0 +1,5 @@
+#program to calculate the some of N numbers
+print("Note:- It will add the number between 1-N. where N is provided by you ")
+n=int(input("Enter the till which yor want to get some :"))
+sum=(n*(n+1))/(2)
+print("The sun  of number from 1 -",n,"is :",sum)
---
+++ Python Basic/Table.py
@@ -0,0 +1,6 @@
+print("Hii, Welcome in the Table Master.I can write the table for you ")
+n=int(input("Enter the number which you want to print table :"))
+
+for i in range(1,11):
+    print(i*n)
+print("Congo! you did it. Enjoy!!")
