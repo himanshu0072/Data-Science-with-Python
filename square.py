2bac9eb27b25050f0b5b2b5610ed8c25c6711057
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-02-07 23:23:16 +0530

    Working with turtle library

---
+++ square.py
@@ -0,0 +1,14 @@
+#turtle is a graphic library
+from turtle import *
+
+speed('slowest')     #speed
+left(120)             #turn left 90 degree
+forward(120)         #then move farward 120 units
+left(60)
+forward(120)
+left(120)
+forward(120)
+left(60)
+forward(120)
+mainloop()
+
