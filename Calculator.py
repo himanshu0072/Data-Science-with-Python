efef6c2d8ca52f0ef21c3e98077f0c0c7d09a7b9
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-02-09 18:36:54 +0530

    Adding floor division in calculator

--- Calculator.py
+++ Calculator.py
@@ -1,20 +1,22 @@
-a=int(input("Enter a number:"))
-print("Enter an operator like.... + , - , * ,  / , %  , **")
-b=input("Enter operator:")
-c=int(input("Enter a number:"))
+a = int(input("Enter a number:"))
+print("Enter an operator like.... + , - , * ,  / , %  , **  , //")
+b = input("Enter operator:")
+c = int(input("Enter a number:"))
 
 match b:
     case "+":
-        print("Sum=",a+c)
+        print("Sum=", a+c)
     case '-':
-        print("Dif=",a-c)
+        print("Dif=", a-c)
     case '*':
-        print("Product=",a*c)
+        print("Product=", a*c)
     case '/':
-        print("Division=",a/c)
+        print("Division=", a/c)
     case '%':
-        print("Remainder=",a%c)
+        print("Remainder=", a % c)
     case '**':
-        print("Square=",a**c)
-    case _:                                
+        print("Square=", a**c)
+    case '//':
+        print("Floor Division=", a//c)
+    case _:
         print(f"{b} is Not a correct operator")
